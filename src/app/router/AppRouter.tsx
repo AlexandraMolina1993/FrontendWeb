7f15ae2aca1fb28bead81a1d4da8c1d3b8f4d4eb
EstefaniaRoggero <roggeroestefania@gmail.com>
2026-09-06 16:14:20 -0300

    feat: agregar rutas para las páginas de carreras y detalle de carrera

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "../../features/Dashboard/views/Dashboard";
+import CarreraDetallePage from "../../features/carreras/pages/CarreraDetallePage";
 import CarrerasAdminPage from "../../features/carreras/pages/CarrerasAdminPage";
+import CarrerasPage from "../../features/carreras/pages/CarrerasPage";
 
 export const AppRouter = () => {
   return (
@@ -9,6 +11,8 @@ export const AppRouter = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/preinscripciones" element={<Dashboard />} />
         <Route path="/dashboard/componentes" element={<Dashboard />} />
+        <Route path="/carreras" element={<CarrerasPage />} />
+        <Route path="/carreras/:id" element={<CarreraDetallePage />} />
         <Route path="/admin/carreras" element={<CarrerasAdminPage />} />
       </Routes>
     </BrowserRouter>
