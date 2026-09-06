42be8f700eda52c1a35d6942546a9745f64b8e01
WindholzCristian <cristian13windholz@gmail.com>
2026-09-06 19:57:49 -0300

    fix: conectar acceso publico y administrativo de contacto

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { LoginPage } from "../../features/auth/pages/LoginPage";
 import Dashboard from "../../features/Dashboard/views/Dashboard";
 import CarrerasAdminPage from "../../features/carreras/pages/CarrerasAdminPage";
 import InstitucionalPage from "../../features/institucional/pages/public/InstitucionalPage";
@@ -13,15 +14,16 @@ import { UsuariosAdminPage } from "../../features/usuarios/pages/UsuariosAdminPa
 import ContactoPage from "../../features/contacto/pages/public/ContactoPage";
 import ContactoAdminPage from "../../features/contacto/pages/admin/ContactoAdminPage";
 import PreinscripcionesPage from "../../features/preinscripciones/pages/PreinscripcionesPage";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/contacto" element={<ContactoPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/preinscripciones" element={<PreinscripcionesPage />} />
         <Route path="/dashboard/componentes" element={<Dashboard />} />
         <Route path="/admin/carreras" element={<CarrerasAdminPage />} />
         <Route path="/institucional" element={<InstitucionalPage />} />
@@ -31,10 +33,13 @@ export const AppRouter = () => {
         <Route path="/admin/sedes" element={<SedesAdminPage />} />
         <Route path="/admin/sedes/nueva" element={<SedeNuevaPage />} />
         <Route path="/admin/usuarios" element={<UsuariosAdminPage />} />
-        <Route path="/admin/contacto" element={<ContactoAdminPage />} />
         <Route path="/admin/noticias" element={<PublicacionesAdminPage />} />
         <Route path="/admin/galeria" element={<GaleriaAdminPage />} />
         <Route path="/admin/galeria/nueva" element={<GaleriaAdminPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard/preinscripciones" element={<PreinscripcionesPage />} />
+          <Route path="/admin/contacto" element={<ContactoAdminPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
