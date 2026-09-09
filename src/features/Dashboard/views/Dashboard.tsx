02158344645f8cfbf41d54384d3555950091fc95
LaraLopez10 <larslopez20@gmail.com>
2026-09-09 20:39:20 -0300

    refactor(dashboard): revertir cambio de singularización en título de sedes

--- src/features/Dashboard/views/Dashboard.tsx
+++ src/features/Dashboard/views/Dashboard.tsx
@@ -167,7 +167,7 @@ export default function Dashboard() {
                   icono: GraduationCap,
                 },
                 {
-                  titulo: "Sede",
+                  titulo: "Sedes",
                   ruta: "/admin/sedes",
                   icono: Building2,
                 },
